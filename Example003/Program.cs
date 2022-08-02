5801aa1f78066eb65ba91a9c53e879bfe568b513
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-02 21:48:41 +0300

    add Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 1;
+int b = 3;
+int c = 7;
+int e = 10;
+int max  = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
