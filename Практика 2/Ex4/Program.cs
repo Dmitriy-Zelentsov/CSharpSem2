370df1d658fd425e598def62f39023b78109fa15
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-08 21:23:13 +0300

    add Ex4

---
+++ Практика 2/Ex4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу,которая принимает на вход число и проверяет кратно ли оно одновременно 7 и 23
+int num1 = 7;
+int num2 = 23;
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine()); 
+if(a % num1 == 0 && a % num2 == 0)
+{
+Console.WriteLine("Да!Кратно");
+}
+else 
+{
+ Console.WriteLine("Нет!");   
+}
